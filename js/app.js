16e25fd61e7f7d891156695dbed3ddf49a09b1aa
Hugo <hhugo.avila97@gmail.com>
2023-10-02 13:08:46 -0300

    feat: ✨Funcion para actualizar la cantidad si el producto es duplicado

--- js/app.js
+++ js/app.js
@@ -125,7 +125,23 @@ function agregarPlatillo(producto) {
     
     //Revisar que la cantidad sea mayor a 0
     if(producto.cantidad > 0) {
-        cliente.pedido = [...pedido, producto]
+        
+        //Comprueba si el elemento ya existe en el array
+        if(pedido.some( articulo => articulo.id === producto.id )) {
+            //El articulo ya existe, actualizar la cantidad
+            const pedidoActualizado = pedido.map( articulo => {
+                if(articulo.id === producto.id) {
+                    articulo.cantidad = producto.cantidad;
+                } 
+                return articulo;
+            })
+            //Se asigna el nuevo array a cliente.pedido
+            cliente.pedido = [...pedidoActualizado]
+    
+        }else {
+            //El articulo no existe, le agregamos al array de pedido
+            cliente.pedido = [...pedido, producto]
+        }
     } else {
         console.log('no es mayor a 0');
     }
