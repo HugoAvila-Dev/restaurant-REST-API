228330b4229f218fca9c77231fe8353cb6ef1b61
Hugo <hhugo.avila97@gmail.com>
2023-10-03 12:32:49 -0300

    feat: ✨ Funcion para calcular propinas

--- js/app.js
+++ js/app.js
@@ -334,6 +334,7 @@ function formularioPropinas() {
     radio10.name = 'propina';
     radio10.value = '10';
     radio10.classList.add('form-check-input');
+    radio10.onclick = calcularPropina;
 
     const radio10Label = document.createElement('LABEL');
     radio10Label.textContent = '10%';
@@ -351,6 +352,7 @@ function formularioPropinas() {
     radio25.name = 'propina';
     radio25.value = '25';
     radio25.classList.add('form-check-input');
+    radio25.onclick = calcularPropina;
 
     const radio25Label = document.createElement('LABEL');
     radio25Label.textContent = '25%';
@@ -368,6 +370,7 @@ function formularioPropinas() {
     radio50.name = 'propina';
     radio50.value = '50';
     radio50.classList.add('form-check-input');
+    radio50.onclick = calcularPropina;
 
     const radio50Label = document.createElement('LABEL');
     radio50Label.textContent = '50%';
@@ -388,5 +391,27 @@ function formularioPropinas() {
     //Agregar al formulario
     formulario.appendChild(divFormulario);
     contenido.appendChild(formulario);
+
+}
+
+function calcularPropina() {
+   
+    const { pedido } = cliente;
+    let subtotal = 0
+
+    //Calcular el subtotal a pagar
+    pedido.forEach(articulo => {
+        const { precio, cantidad } = articulo;
+        subtotal += cantidad * precio;   
+    });
+
+    //Seleccionar el Radio Button con la propina del cliente
+    const propinaSelecionada = document.querySelector('input[name="propina"]:checked').value;
+    
+    //Calcular la propina
+    const propina = ((subtotal * parseInt(propinaSelecionada))/ 100)
+
+    //Calcular el total a pagar
+    const total = subtotal + propina;
 
 }
